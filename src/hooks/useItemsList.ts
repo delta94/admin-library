bd07b7cbe05b97ab0fa0dc2496f3dd4ff8efb61c
Behuamuh <sven1980@list.ru>
2020-07-21 09:39:47 +0300

    added api

--- src/hooks/useItemsList.ts
+++ src/hooks/useItemsList.ts
@@ -14,14 +14,17 @@ interface Result<T> {
   onChangeItem: () => void;
 }
 
-function useItemsList<T>(request: GetItemsRequest, itemName: string): Result<T> {
+function useItemsList<T>(
+  request: GetItemsRequest,
+  itemName: string,
+): Result<T> {
   const [items, setItems] = useState<T[]>([]);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const start = page * rowsPerPage;
-  const end = Math.min((start + rowsPerPage), total);
+  const end = Math.min(start + rowsPerPage, total);
 
   const getItems = async () => {
     const { json, error } = await request(start, rowsPerPage);
