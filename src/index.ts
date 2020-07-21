7a16e6d5cadefae5fce9bc49ba9aec8f9ee31bd4
Behuamuh <sven1980@list.ru>
2020-07-21 10:32:29 +0300

    added components to export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 export * from './hooks';
 export * from './api';
-
-/**
- * "@material-ui/core": "^4.10.1",
-    "@material-ui/icons": "^4.9.1",
-    "@material-ui/lab": "^4.0.0-alpha.55"
- */
+export * from './components';
